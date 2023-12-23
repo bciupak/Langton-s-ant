0fc8a6c7d5e00157c825cc6ce6d92095528f8d1e
bciupak <153275462+bciupak@users.noreply.github.com>
2023-12-23 13:47:47 +0100

    Adding characters.h

---
+++ characters.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <wchar.h>
+
+// border and "Ant" characters
+
+wchar_t* LINE_VERTICAL = L"│";
+wchar_t* LINE_HORIZONTAL = L"─";
+wchar_t* LINE_DOWN_RIGHT = L"┌";
+wchar_t* LINE_DOWN_LEFT = L"┐";
+wchar_t* LINE_UP_RIGHT = L"└";
+wchar_t* LINE_UP_LEFT = L"┘";
+wchar_t* SQUARE_WHITE = L" ";
+wchar_t* SQUARE_BLACK = L"█";
+wchar_t* ARROW_NORTH_WHITE = L"△";
+wchar_t* ARROW_NORTH_BLACK = L"▲";
+wchar_t* ARROW_EAST_WHITE = L"▷";
+wchar_t* ARROW_EAST_BLACK = L"▶";
+wchar_t* ARROW_SOUTH_WHITE = L"▽";
+wchar_t* ARROW_SOUTH_BLACK = L"▼";
+wchar_t* ARROW_WEST_WHITE = L"◁";
+wchar_t* ARROW_WEST_BLACK = L"◀";
+
+
+// use %ls to print wchar_t*
